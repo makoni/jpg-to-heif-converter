302769f7aefde4db05eecfd0aeb81a48de9cf30e
Chris Spradling <chris.spradling@mobileforming.com>
2018-09-05 12:03:12 -0700

    Added support for converting directories, including xcassets and imageset folders

---
+++ JPG to HEIF Converter/FileTypeEnum.swift
@@ -0,0 +1,42 @@
+//
+//  FileTypeEnum.swift
+//  JPG to HEIF Converter
+//
+//  Created by Christopher Spradling on 8/31/18.
+//  Copyright © 2018 Sergey Armodin. All rights reserved.
+//
+
+import Foundation
+
+enum FileType {
+    
+    case image
+    case json
+    case directory
+    case invalid
+    
+    init(_ url: URL) {
+        if FileType.allowedImageTypes.contains(url.pathExtension) {
+            self = .image
+        }
+        else if FileType.directoryTypes.contains(url.pathExtension) {
+            self = .directory
+        }
+        else if url.lastPathComponent.lowercased() == "contents.json" {
+            self = .json
+        }
+        else {
+            self = .invalid
+        }
+        
+    }
+    
+    static var allowedImageTypes: [String] {
+        return ["jpg", "jpeg", "png"]
+    }
+    
+    static var directoryTypes: [String] {
+        return ["xcassets", "imageset", ""]
+    }
+    
+}
--- JPG to HEIF Converter/ViewController.swift
+++ JPG to HEIF Converter/ViewController.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import AVFoundation
+import CoreFoundation
 
 
 /// Converter state
@@ -21,6 +22,7 @@ enum ConverterState: Int {
 	case complete
 }
 
+typealias JSON = [String:Any]
 
 class ViewController: NSViewController {
 	
@@ -111,10 +113,10 @@ extension ViewController {
 		
 		let panel = NSOpenPanel.init()
 		panel.allowsMultipleSelection = true
-		panel.canChooseDirectories = false
+		panel.canChooseDirectories = true
 		panel.canChooseFiles = true
 		panel.isFloatingPanel = true
-		panel.allowedFileTypes = ["jpg", "jpeg", "png"]
+		panel.allowedFileTypes = ["jpg", "jpeg", "png", "xcassets", "imageset"]
 		
 		panel.beginSheetModal(for: self.view.window!) { [weak self] (result) in
 			guard let `self` = self else { return }
@@ -122,55 +124,138 @@ extension ViewController {
 			guard result == .OK else { return }
 			guard panel.urls.isEmpty == false else { return }
 			
-			self.totalImages = panel.urls.count
-			self.converterState = .converting
-			self.processedImages = 0
-			
-			let group = DispatchGroup()
-			
-			let serialQueue = DispatchQueue(label: "me.spaceinbox.jpgtoheifconverter")
-			
-			for imageUrl in panel.urls {
-				group.enter()
-				
-				
-				serialQueue.async { [weak self] in
-					
-					guard let `self` = self else { return }
-					
-					guard let source = CGImageSourceCreateWithURL(imageUrl as CFURL, nil) else { return }
-					guard let image = CGImageSourceCreateImageAtIndex(source, 0, nil) else { return }
-					guard let imageMetadata = CGImageSourceCopyMetadataAtIndex(source, 0, nil) else { return }
-					
-					let pathWithName = imageUrl.deletingPathExtension()
-					guard let outputUrl = URL(string: pathWithName.absoluteString + ".heic") else { return }
-					
-					guard let destination = CGImageDestinationCreateWithURL(
-						outputUrl as CFURL,
-						AVFileType.heic as CFString,
-						1, nil
-					) else {
-						fatalError("unable to create CGImageDestination")
-					}
-					
-					CGImageDestinationAddImageAndMetadata(destination, image, imageMetadata, nil)
-					CGImageDestinationFinalize(destination)
-					
-					DispatchQueue.main.async {
-						self.processedImages += 1
-					}
-					
-					group.leave()
-				}
-				
-			}
-			
-			group.notify(queue: .main, execute: { [weak self] in
-				guard let `self` = self else { return }
-				self.converterState = .complete
-			})
+            self.processItems(panel.urls)
 		}
 	}
 	
+    func processItems(_ urls: [URL]) {
+        converterState = .converting
+        totalImages = 0
+        processedImages = 0
+        
+        let group = DispatchGroup()
+        let serialQueue = DispatchQueue(label: "me.spaceinbox.jpgtoheifconverter")
+        
+        for url in urls {
+            
+            switch FileType(url) {
+            case .image:        convertImage(url, group: group, queue: serialQueue)
+            case .json:         updateContentsFile(url, group: group, queue: serialQueue)
+            case .directory:    processFolder(url, group: group, queue: serialQueue)
+            case .invalid:      continue
+            }
+
+        }
+        
+        group.notify(queue: .main, execute: { [weak self] in
+            guard let `self` = self else { return }
+            self.converterState = .complete
+        })
+        
+    }
+    
+    func processFolder(_ url: URL, group: DispatchGroup, queue: DispatchQueue) {
+        guard case .directory = FileType(url) else { return }
+        
+        let subPaths = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isDirectoryKey])
+        
+        while let path = subPaths?.nextObject() as? URL {
+
+            switch FileType(path) {
+            case .image:
+                convertImage(path, group: group, queue: queue)
+                continue
+            case .json:
+                updateContentsFile(path, group: group, queue: queue)
+            case .directory, .invalid:
+                /* subdirectories' contents are also part of the enumerated sequence, so the directories themselves can be ignored */
+                continue
+            }
+            
+        }
+        
+    }
+    
+    func convertImage(_ imageUrl: URL, group: DispatchGroup, queue: DispatchQueue) {
+        
+        totalImages += 1
+        
+        group.enter()
+        queue.async { [weak self] in
+            
+            guard let `self` = self else { return }
+            
+            guard case .image = FileType(imageUrl) else { return }
+            guard let source = CGImageSourceCreateWithURL(imageUrl as CFURL, nil) else { return }
+            guard let image = CGImageSourceCreateImageAtIndex(source, 0, nil) else { return }
+            guard let imageMetadata = CGImageSourceCopyMetadataAtIndex(source, 0, nil) else { return }
+            
+            let pathWithName = imageUrl.deletingPathExtension()
+            guard let outputUrl = URL(string: pathWithName.absoluteString + ".heic") else { return }
+            
+            guard let destination = CGImageDestinationCreateWithURL(
+                outputUrl as CFURL,
+                AVFileType.heic as CFString,
+                1, nil
+                ) else {
+                    fatalError("unable to create CGImageDestination")
+            }
+            
+            CGImageDestinationAddImageAndMetadata(destination, image, imageMetadata, nil)
+            CGImageDestinationFinalize(destination)
+            
+            DispatchQueue.main.async {
+                self.processedImages += 1
+            }
+            
+            group.leave()
+        }
+        
+    }
+    
 }
 
+extension ViewController {
+    
+    func updateContentsFile(_ url: URL, group: DispatchGroup, queue: DispatchQueue) {
+        guard case .json = FileType(url) else { return }
+        
+        group.enter()
+        queue.async { [weak self] in
+            
+            do {
+                try self?.updateJSONContents(url)
+            } catch let error {
+                print(error)
+            }
+            
+            group.leave()
+        }
+        
+    }
+    
+    private func updateJSONContents(_ url: URL) throws {
+        guard let json = try JSONSerialization.jsonObject(with: Data(contentsOf: url), options: .mutableLeaves) as? JSON else { return }
+        let processed = try JSONSerialization.data(withJSONObject: processJSON(json), options: .prettyPrinted)
+        try processed.write(to: url)
+        
+    }
+    
+    private func processJSON(_ json: JSON) -> JSON {
+        var json = json
+        for (k, v) in json {
+            if k == "filename", let value = v as? String {
+                for type in FileType.allowedImageTypes {
+                    json[k] = value.replacingOccurrences(of: ".\(type)", with: ".heic")
+                }
+            } else if let value = v as? JSON {
+                json[k] = processJSON(value)
+            } else if let values = v as? [JSON] {
+                json[k] = values.compactMap({ return processJSON($0) })
+            }
+        }
+        
+        return json
+    }
+    
+}
